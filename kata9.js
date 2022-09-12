af95790ef9fcc2e91f1c0fd2764f872d8a06ad79
Ralph Unrau <xralphunrau@gmail.com>
2022-09-12 23:37:22 +0000

    Adds funciton for kata

--- kata9.js
+++ kata9.js
@@ -1,3 +1,22 @@
 // We will receive a normal string of words separated with spaces as the input. 
 // Our job is to convert these strings into camel cased strings.
 
+const camelCase = function(str) {
+
+  let newString = '';
+
+  for (let x = 0; x < str.length; x++) {
+    if (str[x] === ' ') {
+      newString += str[x+1].toUpperCase();
+      x += 1;
+    } else {
+      newString += str[x];
+    }
+  }
+
+  return newString;
+};
+
+console.log(camelCase('this is a string'));
+console.log(camelCase('loopy cereal'));
+console.log(camelCase('supercalifragalisticexpialidocious'));

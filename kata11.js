32becccc0c9c36c1d0dba75f7cffadd49a2a203d
Ralph Unrau <xralphunrau@gmail.com>
2022-09-12 23:52:20 +0000

    Adds functions for cone and prism volume

--- kata11.js
+++ kata11.js
@@ -17,5 +17,16 @@ const sphereVolume = function(radius) {
   return Math.pow(radius, 3) * (4/3) * Math.PI;
 };
 
-console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //checks to make sure function is working properly
 
+const coneVolume = function(radius, height) {
+  return (1/3) * Math.PI * Math.pow(radius, 2) * height;
+};
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //checks to make sure function is working properly
+
+const prismVolume = function(height, width, depth) {
+  return depth * width * height;
+};
+
+console.log(prismVolume(3, 4, 5) === 60);

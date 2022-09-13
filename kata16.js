928f90293c9670d178f261fbd38b1be58d1db4ee
Ralph Unrau <xralphunrau@gmail.com>
2022-09-13 01:00:19 +0000

    Adds kata instruction

---
+++ kata16.js
@@ -0,0 +1,7 @@
+// In this exercise, we will be building an advanced case maker that can convert strings into all different kinds of case styles; like camel, pascal, snake, or even kebab.
+// We will still be given an input string to convert. However, this time, we'll also be given a casing style to work with. The following code block will describe all the casing styles to support. We may also receive an array of casing styles, and each of these should be applied.
+// Precedence of each of the casing styles are as follows, values higher in the list should be processed first:
+// camel, pascal, snake, kebab, title
+// vowel, consonant
+// upper, lower
+// Our function should be able to handle all of these cases.

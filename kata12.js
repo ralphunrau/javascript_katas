9378dd6307d036feccec6dd413bb29f6cda6b88e
Ralph Unrau <xralphunrau@gmail.com>
2022-09-13 00:13:13 +0000

    Adds instructions for kata

---
+++ kata12.js
@@ -0,0 +1,13 @@
+// The town festival is tomorrow and the organizers have only just realized that they've booked two bakeries to cater desserts, but only have one kitchen available. Instead of just choosing one bakery, let's help them figure out a way to work together.
+
+// Both of the bakeries have a specialty, so they each have a stock of specific ingredients.
+
+// Lucky for the festival organizers, we've found a recipe book in the town library with TONS of fusion recipes, unfortunately it's thousands of pages long and we don't have much time. Let's write a function that helps determine which recipes match the ingredients that both bakeries have in stock.
+// We need to complete a function called chooseRecipe(), which will receive three parameters: - An array of ingredients in stock at Bakery A - An array of ingredients in stock at Bakery B - An array of recipe objects. Each recipe has a name property(string) and an ingredient property(array)
+// We are limiting our search to two ingredient recipes. We want to find a recipe that utilizes one ingredient from Bakery A and one from Bakery B.
+
+// Our chooseRecipe() function should return the name of the correct recipe.
+
+const chooseRecipe = function(aStock, bStock, recipeArray) {
+
+};

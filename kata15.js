ff75fe3a8c66a30bac2919292e01599d1a8af8e0
Ralph Unrau <xralphunrau@gmail.com>
2022-09-13 00:58:24 +0000

    Implements function to organize instructors

--- kata15.js
+++ kata15.js
@@ -1,2 +1,30 @@
 // In this exercise, we will be given a list of instructors and we will create a single object to organize them based on their course.
 
+const organizeInstructors = function(instructorArray) {
+  let newObj = {};
+  let currentCourse = '';
+
+  for (let x of instructorArray) {
+    currentCourse = x.course;
+    if (newObj[currentCourse]) {
+      newObj[currentCourse].push(x.name);
+    } else {
+      newObj[currentCourse] = [x.name]
+    }
+  }
+  return newObj;
+};
+
+// checks if function is working properly
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
